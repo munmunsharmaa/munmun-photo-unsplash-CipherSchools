100c94f12ad05df20c8d0381b614c3ecaa81e282
Munmun <88223127+munmunsharmaa@users.noreply.github.com>
2022-07-17 22:32:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import SearchPhotos from "./searchPhotos"
+
+function App() {
+  return (
+    <div className="App">
+     
+ <div className="container">
+        <h1 className="title">React Photo Search </h1>
+        <SearchPhotos /> 
+        </div>
+    </div>
+  );
+}
+export default App;
